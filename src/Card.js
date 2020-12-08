1173b497f117533fe8f868bfc6afa71309f8a403
mahongquan <mahongquan@sina.com>
2020-12-08 19:21:38 +0800

    chinese

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,33 @@
 import React , { Fragment }from 'react';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-
+import { Font,Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+Font.register({
+  family: 'SimHei',
+  src: '/SimHei.ttf',
+});
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'SimHei',
+  },
+});
 export default class A4Lian extends React.Component {
   render() {
     return (
       <View>
-          <Text>hello北京科技大学预收款凭条&emsp;&emsp;&emsp;No&emsp;{this.props.start}
+          <Text>hello&emsp;&emsp;&emsp;No&emsp;{this.props.start}
           </Text>
-          <Text>
+          <Text  style={styles.text}>
             （不作报销凭证）
           </Text>
-            <Text>今收到</Text>
-            <Text>交&emsp;来</Text>
-            <Text>人民币（大写）</Text>
-            <Text>￥</Text>
-            <Text>收款单位</Text>
+            <Text style={styles.text}>今收到</Text>
+            <Text style={styles.text}>交&emsp;来</Text>
+            <Text style={styles.text}>人民币（大写）</Text>
+            <Text style={styles.text}>￥</Text>
+            <Text style={styles.text}>收款单位</Text>
             <Text style={{ margin: '0 0 0 38mm' }}>收款人</Text>
-            <Text>(公章)</Text>
-            <Text>(签章)</Text>
-            <Text>&emsp;&emsp;&emsp;年&emsp;月&emsp;日</Text>
-        <Text>
+            <Text style={styles.text}>(公章)</Text>
+            <Text style={styles.text}>(签章)</Text>
+            <Text style={styles.text}>&emsp;&emsp;&emsp;年&emsp;月&emsp;日</Text>
+        <Text style={styles.text}>
           {this.props.lian}
         </Text>
       </View>
